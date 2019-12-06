5c0dffd42f7f141b03a70385d5709418c1cfb045
Matt Burke <mlb452@cornell.edu>
2019-12-06 18:10:53 -0500

    update experiment scripts

--- experiments/e1/run.sh
+++ experiments/e1/run.sh
@@ -2,7 +2,7 @@ source experiments/common.sh
 
 ## Local SGD
 remote 'r' 'killall -9 python3'
-remote 'r' 'python3 dist.py --config_file experiments/e1/sgd.json --local'
+remote 'r' 'python3 dist.py --config_file experiments/e1/sgd.json --local &> log.out'
 ##
 
 ## Parallel SGD 1
@@ -11,14 +11,14 @@ remote 'client-0-0' 'killall -9 python3'
 remote 'client-0-1' 'killall -9 python3'
 remote 'client-0-2' 'killall -9 python3'
 
-remote 'r' 'python3 dist.py --config_file experiments/e1/psgd1.json --worker_idx 0 ' &
+remote 'r' 'python3 dist.py --config_file experiments/e1/psgd1.json --worker_idx 0 &> log.out' &
 master=$!
 sleep 0.7
-remote 'client-0-0' 'python3 dist.py --config_file experiments/e1/psgd1.json --worker_idx 1' &
+remote 'client-0-0' 'python3 dist.py --config_file experiments/e1/psgd1.json --worker_idx 1 &> log.out' &
 sleep 0.7
-remote 'client-0-1' 'python3 dist.py --config_file experiments/e1/psgd1.json --worker_idx 2 ' &
+remote 'client-0-1' 'python3 dist.py --config_file experiments/e1/psgd1.json --worker_idx 2 &> log.out' &
 sleep 0.7
-remote 'client-0-2' 'python3 dist.py --config_file experiments/e1/psgd1.json --worker_idx 3' &
+remote 'client-0-2' 'python3 dist.py --config_file experiments/e1/psgd1.json --worker_idx 3 &> log.out' &
 wait $master
 ##
 
@@ -28,14 +28,14 @@ remote 'client-0-0' 'killall -9 python3'
 remote 'client-0-1' 'killall -9 python3'
 remote 'client-0-2' 'killall -9 python3'
 
-remote 'r' 'python3 dist.py --config_file experiments/e1/psgd2.json --worker_idx 0 ' &
+remote 'r' 'python3 dist.py --config_file experiments/e1/psgd2.json --worker_idx 0 &> log.out' &
 master=$!
 sleep 0.7
-remote 'client-0-0' 'python3 dist.py --config_file experiments/e1/psgd2.json --worker_idx 1' &
+remote 'client-0-0' 'python3 dist.py --config_file experiments/e1/psgd2.json --worker_idx 1 &> log.out' &
 sleep 0.7
-remote 'client-0-1' 'python3 dist.py --config_file experiments/e1/psgd2.json --worker_idx 2 ' &
+remote 'client-0-1' 'python3 dist.py --config_file experiments/e1/psgd2.json --worker_idx 2 &> log.out' &
 sleep 0.7
-remote 'client-0-2' 'python3 dist.py --config_file experiments/e1/psgd2.json --worker_idx 3' &
+remote 'client-0-2' 'python3 dist.py --config_file experiments/e1/psgd2.json --worker_idx 3 &> log.out' &
 wait $master
 ##
 
@@ -45,14 +45,14 @@ remote 'client-0-0' 'killall -9 python3'
 remote 'client-0-1' 'killall -9 python3'
 remote 'client-0-2' 'killall -9 python3'
 
-remote 'r' 'python3 dist.py --config_file experiments/e1/psgd10.json --worker_idx 0 ' &
+remote 'r' 'python3 dist.py --config_file experiments/e1/psgd10.json --worker_idx 0 >& log.out' &
 master=$!
 sleep 0.7
-remote 'client-0-0' 'python3 dist.py --config_file experiments/e1/psgd10.json --worker_idx 1' &
+remote 'client-0-0' 'python3 dist.py --config_file experiments/e1/psgd10.json --worker_idx 1 &> log.out' &
 sleep 0.7
-remote 'client-0-1' 'python3 dist.py --config_file experiments/e1/psgd10.json --worker_idx 2 ' &
+remote 'client-0-1' 'python3 dist.py --config_file experiments/e1/psgd10.json --worker_idx 2 &> log.out' &
 sleep 0.7
-remote 'client-0-2' 'python3 dist.py --config_file experiments/e1/psgd10.json --worker_idx 3' &
+remote 'client-0-2' 'python3 dist.py --config_file experiments/e1/psgd10.json --worker_idx 3 &> log.out' &
 wait $master
 ##
 
--- experiments/retrieve.sh
+++ experiments/retrieve.sh
@@ -1,3 +1,3 @@
-for s in r client-0-0 client-0-1 client-0-2 client-0-3 client-0-4 client-0-5 client-0-6; do
+for s in r; do
 rsync -avz $u@$s.psgd.morty-pg0.utah.cloudlab.us:experiments .
 done
