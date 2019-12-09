1bdc974586abb35e00118e028f68356ae85c4955
Matt Burke <mlb452@cornell.edu>
2019-12-09 16:28:14 -0500

    update epx

--- experiments/e2/run.sh
+++ experiments/e2/run.sh
@@ -1,19 +1,53 @@
 source experiments/common.sh
 
-## Parallel SGD 1
+## Parallel SGD 2
 remote 'r' 'killall -9 python3'
 remote 'client-0-0' 'killall -9 python3'
 remote 'client-0-1' 'killall -9 python3'
 remote 'client-0-2' 'killall -9 python3'
 
-remote 'r' 'python3 dist.py --config_file experiments/e2/psgd.json --worker_idx 0 &> log.out' &
+remote 'r' 'python3 dist.py --config_file experiments/e2/psgd2.json --worker_idx 0 &> log.out' &
 master=$!
 sleep 0.7
-remote 'client-0-0' 'python3 dist.py --config_file experiments/e2/psgd.json --worker_idx 1 &> log.out' &
+remote 'client-0-0' 'python3 dist.py --config_file experiments/e2/psgd2.json --worker_idx 1 &> log.out' &
 sleep 0.7
-remote 'client-0-1' 'python3 dist.py --config_file experiments/e2/psgd.json --worker_idx 2 &> log.out' &
+remote 'client-0-1' 'python3 dist.py --config_file experiments/e2/psgd2.json --worker_idx 2 &> log.out' &
 sleep 0.7
-remote 'client-0-2' 'python3 dist.py --config_file experiments/e2/psgd.json --worker_idx 3 &> log.out' &
+remote 'client-0-2' 'python3 dist.py --config_file experiments/e2/psgd2.json --worker_idx 3 &> log.out' &
+wait $master
+##
+
+## Parallel SGD 200
+remote 'r' 'killall -9 python3'
+remote 'client-0-0' 'killall -9 python3'
+remote 'client-0-1' 'killall -9 python3'
+remote 'client-0-2' 'killall -9 python3'
+
+remote 'r' 'python3 dist.py --config_file experiments/e2/psgd200.json --worker_idx 0 &> log.out' &
+master=$!
+sleep 0.7
+remote 'client-0-0' 'python3 dist.py --config_file experiments/e2/psgd200.json --worker_idx 1 &> log.out' &
+sleep 0.7
+remote 'client-0-1' 'python3 dist.py --config_file experiments/e2/psgd200.json --worker_idx 2 &> log.out' &
+sleep 0.7
+remote 'client-0-2' 'python3 dist.py --config_file experiments/e2/psgd200.json --worker_idx 3 &> log.out' &
+wait $master
+##
+
+## Parallel SGD 450
+remote 'r' 'killall -9 python3'
+remote 'client-0-0' 'killall -9 python3'
+remote 'client-0-1' 'killall -9 python3'
+remote 'client-0-2' 'killall -9 python3'
+
+remote 'r' 'python3 dist.py --config_file experiments/e2/psgd450.json --worker_idx 0 &> log.out' &
+master=$!
+sleep 0.7
+remote 'client-0-0' 'python3 dist.py --config_file experiments/e2/psgd450.json --worker_idx 1 &> log.out' &
+sleep 0.7
+remote 'client-0-1' 'python3 dist.py --config_file experiments/e2/psgd450.json --worker_idx 2 &> log.out' &
+sleep 0.7
+remote 'client-0-2' 'python3 dist.py --config_file experiments/e2/psgd450.json --worker_idx 3 &> log.out' &
 wait $master
 ##
 
